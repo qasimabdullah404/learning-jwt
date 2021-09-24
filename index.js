a66076eb54f7144399f9d461dab36c6cb276b446
Qasim Abdullah <qasimabdullah404@gmail.com>
2021-09-24 22:59:05 +0500

    mongoose and dotenv setup

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, () => console.log('Connected to MongoDB'))
 
 const authRoute = require('./routes/auth')
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 // Middleware
 app.use('/api/user', authRoute)
 
-app.listen(3000, () => console.log(`Server up and running...`))
+app.listen(3000, () => console.log(`Server up and running on ${PORT}...`))
